8ed9980ef2212004dcb37476f2b162d9fd7644d6
Saifur Rahman <saifur.rahman18@gmail.com>
2019-08-22 17:23:47 +0530

    Add files via upload

---
+++ sendMultipleSms.php
@@ -0,0 +1,41 @@
+<?php
+
+$curl = curl_init();
+$apiKey=""; //Enter The API Key Here
+$senderId="PNGSMS"; //Your SenderId. PNGSMS is default senderId
+$mobileNumber="7308741782,8414084008"; //10 digit phone number separated by comma (,)
+$language="1";
+$product="1";
+
+/*           
+Language:   1 - English, 2 - Unicode (Regional Language)
+Product :   1 - Transactional, 2 - Promotional
+*/
+
+$message="Hello World";
+
+$message=urlencode($message);// Encode the message to send it through URL
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://www.pingsms.in/api/sendmultiplesms?key=".$apiKey."&sender=".$senderId."&mobile=".$mobileNumber."&language=".$language."&product=".$product."&message=".$message,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "X-Authorization: ".$apiKey
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
