51ff5708b7344425821be97541fde3b79eb2d780
Saifur Rahman <saifur.rahman18@gmail.com>
2021-06-12 18:27:03 +0530

    Update sendSingleSms.php

--- sendSingleSms.php
+++ sendSingleSms.php
@@ -7,18 +7,21 @@ $senderId="PNGSMS"; //Your SenderId. PNGSMS is default senderId
 $mobileNumber=""; //10 digit phone number
 $language="1";
 $product="1";
+$template="289329228923730";
 
 /*           
 Language:   1 - English, 2 - Unicode (Regional Language)
 Product :   1 - Transactional, 2 - Promotional
+Template : Your Approved Template ID
+(Your message sould correspond with approved template ID)
 */
 
-$message="Hello World";
+$message="Hello World"; //Approved Template Message
 
 $message=urlencode($message);// Encode the message to send it through URL
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://www.pingsms.in/api/sendsms?key=".$apiKey."&sender=".$senderId."&mobile=".$mobileNumber."&language=".$language."&product=".$product."&message=".$message,
+  CURLOPT_URL => "https://www.pingsms.in/api/sendsms?key=".$apiKey."&sender=".$senderId."&mobile=".$mobileNumber."&language=".$language."&product=".$product."&message=".$message."&template=".$template,
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
